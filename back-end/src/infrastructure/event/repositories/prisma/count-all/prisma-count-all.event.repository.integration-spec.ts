cbec89d57a410143253c5d5ad18eb8ee23a14353
kistgab <gabrielkist02@gmail.com>
2024-04-23 20:13:46 -0300

    test: add CountAllEventsRepository implementation with Prisma

---
+++ back-end/src/infrastructure/event/repositories/prisma/count-all/prisma-count-all.event.repository.integration-spec.ts
@@ -0,0 +1,71 @@
+import { EventType, PrismaClient } from '@prisma/client';
+import { beforeEach } from 'node:test';
+import { PrismaCountAllEventsRepository } from 'src/infrastructure/event/repositories/prisma/count-all/prisma-count-all.event.repository';
+
+describe('PrismaCountAll Event Repository', () => {
+  let prismaCountAllEventRepository: PrismaCountAllEventsRepository;
+  let prismaClient: PrismaClient;
+
+  beforeAll(async () => {
+    prismaClient = new PrismaClient();
+    await prismaClient.$connect();
+    prismaCountAllEventRepository = new PrismaCountAllEventsRepository(
+      prismaClient,
+    );
+  });
+
+  beforeEach(async () => {
+    await prismaClient.event.deleteMany();
+    await prismaClient.event.createMany({
+      data: [
+        {
+          contact_email: 'contact_email',
+          date: new Date(),
+          name: 'name',
+          placeId: 'placeId',
+          type: EventType.Presentation,
+          contact_phone: 'contact_phone',
+          id: 'id',
+        },
+        {
+          contact_email: 'other_contact_email',
+          date: new Date('2023-03-03'),
+          name: 'other_name',
+          placeId: 'placeId2',
+          type: EventType.Show,
+          contact_phone: 'other_contact_phone',
+          id: 'other_id',
+        },
+      ],
+    });
+  });
+
+  afterAll(async () => {
+    await prismaClient.$disconnect();
+  });
+
+  it('should return the number of all events', async () => {
+    const events = await prismaClient.event.findMany();
+    const count = await prismaCountAllEventRepository.countAll();
+
+    expect(count).toBe(events.length);
+  });
+
+  it('should return 0 when there are no events', async () => {
+    const events = await prismaClient.event.findMany();
+    const count = await prismaCountAllEventRepository.countAll({
+      name: 'not_found_name',
+    });
+
+    expect(count).toBe(events.length);
+  });
+
+  it('should return the count of only the filtered events', async () => {
+    const events = await prismaClient.event.findMany();
+    const count = await prismaCountAllEventRepository.countAll({
+      name: 'name',
+    });
+
+    expect(count).toBe(events.filter((event) => event.name === 'name').length);
+  });
+});
