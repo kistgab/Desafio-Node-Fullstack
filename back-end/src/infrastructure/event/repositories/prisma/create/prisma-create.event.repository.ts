156b6cbe5d9da437c25796cb3fde947797633f23
kistgab <gabrielkist02@gmail.com>
2024-04-24 04:40:03 -0300

    feat: add PrismaCreateEventRepository

---
+++ back-end/src/infrastructure/event/repositories/prisma/create/prisma-create.event.repository.ts
@@ -0,0 +1,22 @@
+import { EventEntity } from '@domain/event/entity/event.entity';
+import { CreateEventRepository } from '@domain/event/repositories/create.event.repository';
+import { PrismaClient } from '@prisma/client';
+
+export class PrismaCreateEventRepository implements CreateEventRepository {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async create(event: EventEntity): Promise<void> {
+    await this.prisma.event.create({
+      data: {
+        contact_email: event.contact.email,
+        contact_phone: event.contact.phone,
+        startDate: event.duration.startsAt,
+        endDate: event.duration.endsAt,
+        name: event.name,
+        placeId: event.place.id,
+        type: event.type,
+        id: event.id,
+      },
+    });
+  }
+}
