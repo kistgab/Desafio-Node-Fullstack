33f732f5fff59b7747c42bd7eac51d46097939cd
kistgab <gabrielkist02@gmail.com>
2024-04-24 11:48:50 -0300

    refactor: add params  to change the result of function mockPlaceEntity

--- back-end/__tests__/utils/place.utils.ts
+++ back-end/__tests__/utils/place.utils.ts
@@ -5,31 +5,38 @@ import {
   PlaceFactory,
 } from '@domain/place/factory/place.factory';
 
-export function mockPlaceEntity(): PlaceEntity {
-  return PlaceFactory.create(mockPlaceEntityProps());
+export function mockPlaceEntity(
+  props?: Partial<PlaceEntityProps>,
+): PlaceEntity {
+  return PlaceFactory.create(mockPlaceEntityProps(props));
 }
 
-export function mockPlaceEntityProps(): PlaceEntityProps {
+export function mockPlaceEntityProps(
+  props?: Partial<PlaceEntityProps>,
+): PlaceEntityProps {
   return {
-    id: 'any_id',
-    name: 'any_name',
-    nickname: 'any_nickname',
-    type: PlaceType.Stadium,
-    address: {
-      city: 'any_city',
-      state: 'any_state',
-      zipCode: 'any_zipcode',
-      line: 'any_line',
-      complement: 'any_compliment',
+    ...{
+      id: 'any_id',
+      name: 'any_name',
+      nickname: 'any_nickname',
+      type: PlaceType.Stadium,
+      address: {
+        city: 'any_city',
+        state: 'any_state',
+        zipCode: 'any_zipcode',
+        line: 'any_line',
+        complement: 'any_compliment',
+      },
+      contact: {
+        mail: 'mail@mail.com',
+        phone: '99999999999',
+      },
+      entries: [],
+      ticketGates: [],
+      cnpj: 'any_cnpj',
+      createdAt: new Date('2023-01-29'),
+      updatedAt: new Date('2024-04-04'),
     },
-    contact: {
-      mail: 'mail@mail.com',
-      phone: '99999999999',
-    },
-    entries: [],
-    ticketGates: [],
-    cnpj: 'any_cnpj',
-    createdAt: new Date('2023-01-29'),
-    updatedAt: new Date('2024-04-04'),
+    ...props,
   };
 }
