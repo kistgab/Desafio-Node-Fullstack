de91dd256d04b5da845a6a4e8503e13a469d1cfa
kistgab <gabrielkist02@gmail.com>
2024-04-24 15:55:21 -0300

    feat: add ListAllPlaces controller

--- back-end/src/infrastructure/place/factories/usecases.factory.ts
+++ back-end/src/infrastructure/place/factories/usecases.factory.ts
@@ -2,9 +2,12 @@ import { PrismaClient } from '@prisma/client';
 import { CreatePlaceUseCase } from '@usecases/place/create/create.place.usecase';
 import { DeletePlaceUseCase } from '@usecases/place/delete/delete.place.usecase';
 import { DetailPlaceUseCase } from '@usecases/place/detail/detail.place.usecase';
+import { ListAllPlacesUseCase } from '@usecases/place/list-all/list-all.place.usecase';
+import { PrismaCountAllPlacesRepository } from 'src/infrastructure/place/repositories/prisma/count-all/prisma-count-all.place.repository';
 import { PrismaCreatePlaceRepository } from 'src/infrastructure/place/repositories/prisma/create/prisma-create.place.repository';
 import { PrismaDeletePlaceRepository } from 'src/infrastructure/place/repositories/prisma/delete/prisma-delete.place.repository';
 import { PrismaPlaceExistsByNameRepository } from 'src/infrastructure/place/repositories/prisma/exists-by-name/prisma-exists-by-name.place.repository';
+import { PrismaFindAllPlacesRepository } from 'src/infrastructure/place/repositories/prisma/find-all/prisma-find-all.place.repository';
 import { PrismaFindPlaceByIdRepository } from 'src/infrastructure/place/repositories/prisma/find-by-id/prisma-find-by-id.place.repository';
 
 export abstract class PlaceUseCasesFactory {
@@ -38,5 +41,19 @@ export abstract class PlaceUseCasesFactory {
       prismaClient,
     );
     return new DetailPlaceUseCase(findPlaceByIdRepository);
+  }
+
+  static async listAll(): Promise<ListAllPlacesUseCase> {
+    const prismaClient = new PrismaClient();
+    const findAllPlacesRepository = new PrismaFindAllPlacesRepository(
+      prismaClient,
+    );
+    const countAllPlacesRepository = new PrismaCountAllPlacesRepository(
+      prismaClient,
+    );
+    return new ListAllPlacesUseCase(
+      findAllPlacesRepository,
+      countAllPlacesRepository,
+    );
   }
 }
