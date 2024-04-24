6eb50f3c015cf3aa1b45b300b6f38c6171d41947
kistgab <gabrielkist02@gmail.com>
2024-04-24 16:47:03 -0300

    fix: make the right logic in IsPlaceAvailableRepository

--- back-end/src/infrastructure/place/repositories/prisma/is-place-available-at/prisma-is-place-available-at.place.repository.ts
+++ back-end/src/infrastructure/place/repositories/prisma/is-place-available-at/prisma-is-place-available-at.place.repository.ts
@@ -15,15 +15,30 @@ export class PrismaIsPlaceAvailableAtRepository
     const eventsInPeriodCount = await this.prisma.event.count({
       where: {
         place_id: placeId,
-        AND: [
+        OR: [
+          // Starts in the middle, ends in the middle
+          {
+            startDate: { gte: startDate, lte: endDate },
+            endDate: { lte: endDate },
+          },
+          // Starts in the middle, ends after
+          {
+            startDate: { gte: startDate, lte: endDate },
+            endDate: { gte: endDate },
+          },
+          // Starts before, ends in the middle
+          {
+            startDate: { lte: startDate },
+            endDate: { gte: startDate, lte: endDate },
+          },
+          // Starts before, ends after
           {
             startDate: { lte: startDate },
             endDate: { gte: endDate },
           },
-          { startDate: { gte: startDate }, endDate: { lte: endDate } },
         ],
       },
     });
-    return eventsInPeriodCount > 0;
+    return eventsInPeriodCount === 0;
   }
 }
