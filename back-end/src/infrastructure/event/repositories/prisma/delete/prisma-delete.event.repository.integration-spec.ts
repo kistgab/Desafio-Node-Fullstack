cc93b09f7dfed53b0222d5c56fc9549a9d73b148
kistgab <gabrielkist02@gmail.com>
2024-04-24 04:49:55 -0300

    test: add PrismaDeleteEventRepository

---
+++ back-end/src/infrastructure/event/repositories/prisma/delete/prisma-delete.event.repository.integration-spec.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from '@prisma/client';
+import {
+  clearDatabase,
+  mockEventModelData,
+  mockPlaceModelData,
+} from '@test/utils/prisma.utils';
+import { beforeEach } from 'node:test';
+import { PrismaDeleteEventRepository } from 'src/infrastructure/event/repositories/prisma/delete/prisma-delete.event.repository';
+
+describe('PrismaDelete Event Repository', () => {
+  let prismaDeleteEventRepository: PrismaDeleteEventRepository;
+  let prismaClient: PrismaClient;
+
+  beforeAll(async () => {
+    prismaClient = new PrismaClient();
+    await prismaClient.$connect();
+    await clearDatabase(prismaClient);
+    await prismaClient.place.create({
+      data: mockPlaceModelData(),
+    });
+    prismaDeleteEventRepository = new PrismaDeleteEventRepository(prismaClient);
+  });
+
+  beforeEach(async () => {
+    await prismaClient.event.deleteMany();
+  });
+
+  afterAll(async () => {
+    await clearDatabase(prismaClient);
+    await prismaClient.$disconnect();
+  });
+
+  it('should delete the Event from database', async () => {
+    const insertedEvent = await prismaClient.event.create({
+      data: mockEventModelData(),
+    });
+    await prismaDeleteEventRepository.delete(insertedEvent.id);
+
+    const wasEventDeleted =
+      (await prismaClient.event.count({ where: { id: insertedEvent.id } })) ===
+      0;
+    expect(wasEventDeleted).toBeTruthy();
+  });
+});
