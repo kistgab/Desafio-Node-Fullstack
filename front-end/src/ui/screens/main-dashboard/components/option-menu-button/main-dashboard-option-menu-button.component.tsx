1e689cf8d1dc8d27971f4670b82df99810348620
kistgab <gabrielkist02@gmail.com>
2024-04-24 22:27:20 -0300

    feat: add main dashboard buttons

---
+++ front-end/src/ui/screens/main-dashboard/components/option-menu-button/main-dashboard-option-menu-button.component.tsx
@@ -0,0 +1,50 @@
+import { Button } from "@chakra-ui/button";
+import { Image } from "@chakra-ui/image";
+import { Box, Flex, Heading, Text } from "@chakra-ui/layout";
+import { Link } from "react-router-dom";
+
+type Props = {
+  icon: string;
+  title: string;
+  text: string;
+  buttonText: string;
+  bgColor: string;
+  href: string;
+};
+
+export function MainDashboardOptionMenuButton({
+  buttonText,
+  icon,
+  text,
+  title,
+  bgColor,
+  href,
+}: Props) {
+  return (
+    <Box bg={bgColor} p={"1.5rem"} borderRadius={"16px"}>
+      <Flex
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        gap={"1.5rem"}
+      >
+        <Box flex={2}>
+          <Flex gap={".5rem"}>
+            <Image sizes={"sm"} src={icon} alt={`${title} icon`} />
+            <Heading>{title}</Heading>
+          </Flex>
+          <Text>{text}</Text>
+        </Box>
+        <Link to={href}>
+          <Button
+            variant={"solid"}
+            bg={"#CAD6EC"}
+            color={"#10141D"}
+            _hover={{ bg: "#828fa8", color: "white" }}
+          >
+            {buttonText}
+          </Button>
+        </Link>
+      </Flex>
+    </Box>
+  );
+}
--- front-end/src/ui/screens/main-dashboard/main-dashboard.screen.tsx
+++ front-end/src/ui/screens/main-dashboard/main-dashboard.screen.tsx
@@ -1,6 +1,9 @@
-import { Box, Center } from "@chakra-ui/layout";
+import EventIcon from "@assets/icons/event-icon.svg";
+import FestivalIcon from "@assets/icons/festival-icon.svg";
+import { Box, Center, Flex } from "@chakra-ui/layout";
 import { Header } from "@ui/components/header/header.component";
 import { MainDashboardTitle } from "@ui/screens/main-dashboard/components/header/main-dashboard-title.component";
+import { MainDashboardOptionMenuButton } from "@ui/screens/main-dashboard/components/option-menu-button/main-dashboard-option-menu-button.component";
 
 export function MainDashboardScreen() {
   return (
@@ -9,6 +12,33 @@ export function MainDashboardScreen() {
       <Center>
         <Box w={"80%"} p={"3.25rem 5.125rem 1.5rem"}>
           <MainDashboardTitle userName="Mariana" />
+          <Flex
+            mt={"1.5rem"}
+            gap={"1.5rem"}
+            justifyContent={"center"}
+            flexWrap={"wrap"}
+          >
+            <Box>
+              <MainDashboardOptionMenuButton
+                buttonText="Conferir locais"
+                icon={FestivalIcon}
+                text="Confira todo os locais cadastrados!"
+                title="Locais"
+                bgColor="#2F3B28"
+                href="/locais"
+              />
+            </Box>
+            <Box>
+              <MainDashboardOptionMenuButton
+                buttonText="Conferir eventos"
+                icon={EventIcon}
+                text="Confira todo os eventos cadastrados!"
+                title="Eventos"
+                bgColor="#461527"
+                href="/eventos"
+              />
+            </Box>
+          </Flex>
         </Box>
       </Center>
     </>
